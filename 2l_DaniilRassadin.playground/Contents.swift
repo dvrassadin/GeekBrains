3af04b262a4522623b2183a3952a0f22f3bc4041
Daniil Rassadin <daniil@rassadin.org>
2022-05-26 19:26:19 +0300

    Задание 2.5

--- 2l_DaniilRassadin.playground/Contents.swift
+++ 2l_DaniilRassadin.playground/Contents.swift
@@ -4,12 +4,56 @@ import UIKit
 //2. Написать функцию, которая определяет, делится ли число без остатка на 3.
 
 func multipleOf(number: Int, factor: Int) -> Bool {
-let b = number % factor == 0 ? true : false
-return b
+return number % factor == 0
 }
 
 let a = multipleOf(number: 67, factor: 3)
 print(a ? "Кратное" : "Некратное")
 
+
 //3. Создать возрастающий массив из 100 чисел.
 
+var array = [Int]()
+
+for i in (0...99) {
+    array.append(i + 1)
+}
+
+print(array)
+print("––––––––––")
+
+
+//4. Удалить из этого массива все четные числа и все числа, которые не делятся на 3.
+
+for i in array {
+    if multipleOf(number: i, factor: 2) == true {
+        array.remove(at: array.firstIndex(of: i)!) //функцию взял из методички и не понял как она работает, буду рад если объясните на уроке
+    } else if multipleOf(number: i, factor: 3) == true { //не понял почему работает true и в первом и во втором случае, по моей логике должно работать false в это случае
+        array.remove(at: array.firstIndex(of: i)!)
+    }
+}
+
+print(array)
+print("––––––––––")
+
+
+//5. * Написать функцию, которая добавляет в массив новое число Фибоначчи, и добавить при помощи нее 50 элементов.
+
+var arrayFib = [0, 1]
+
+for i in 0...49 {
+    arrayFib.append(arrayFib[0 + i] + arrayFib[1 + i])
+}
+
+print(arrayFib)
+
+//6. * Заполнить массив элементов различными простыми числами.
+
+var arrayPrime = [Int]()
+var p = 2
+
+for i in (1...100) {
+    arrayPrime.append(i + 1)
+}
+
+//Больше не осилил. =(
