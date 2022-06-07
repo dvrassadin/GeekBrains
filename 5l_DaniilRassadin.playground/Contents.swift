68075ce85a2d85b1dd0cfedcf3e1931a059780cc
Daniil Rassadin <daniil@rassadin.org>
2022-06-07 15:38:27 +0300

    5 заданий

--- 5l_DaniilRassadin.playground/Contents.swift
+++ 5l_DaniilRassadin.playground/Contents.swift
@@ -1,136 +1,148 @@
 import UIKit
 
-enum EngineStatus {
-    case on, off
-}
+ enum EngineStatus {
+     case on, off
+ }
 
-enum WindowsStatus {
-    case open, close
-}
+ enum WindowsStatus {
+     case open, close
+ }
 
-enum Action {
-    case fillTank(Double)
-    case changeTires(String)
-}
+ enum Action {
+     case fillTank(Double)
+     case changeTires(String)
+ }
 
-protocol Car {
-    var model: String {get}
-    var manufactureYear: Int {get}
-    var engineStatus: EngineStatus {get set}
-    var windowsStatus: WindowsStatus {get set}
-    
-    func perform(action: Action)
-}
+ protocol Car {
+     var model: String {get}
+     var manufactureYear: Int {get}
+     var engineStatus: EngineStatus {get set}
+     var windowsStatus: WindowsStatus {get set}
 
-extension Car {
-    mutating func switchEngine (action: EngineStatus) {
-        if action == .on && engineStatus == .on {
-            print("Двигатель уже запущен")
-        } else if action == .off && engineStatus == .off {
-            print("Двигатель уже сотановлен")
-        } else {
-            return engineStatus = action
-        }
-    }
-}
+     func perform(action: Action)
+ }
 
-extension Car {
-    mutating func switchWindows (action: WindowsStatus) {
-        if action == .open && windowsStatus == .open {
-            print("Окна уже открыты")
-        } else if action == .close && windowsStatus == .close {
-            print("Окна уже закрыты")
-        } else {
-            return windowsStatus = action
-        }
-    }
-}
+ extension Car {
+     mutating func switchEngine (action: EngineStatus) {
+         if action == .on && engineStatus == .on {
+             print("Двигатель уже запущен")
+         } else if action == .off && engineStatus == .off {
+             print("Двигатель уже сотановлен")
+         } else {
+             return engineStatus = action
+         }
+     }
+ }
 
-
-class TankCar: Car {
-    var model: String
-    var manufactureYear: Int
-    var engineStatus: EngineStatus = .off
-    var windowsStatus: WindowsStatus = .close
-    let tankVolume: Double
-    var filledTankVolume: Double = 0
-    
-    func perform(action: Action) {
-        switch action {
-        case .fillTank(let liquid):
-            let expectedVolume = liquid + filledTankVolume
-            if expectedVolume > tankVolume {
-                print("Нет столько места")
-            } else if expectedVolume < 0 {
-                break
-            } else {
-                filledTankVolume = expectedVolume
-                print("Цистерна заполена на \(filledTankVolume) из \(tankVolume).")
-            }
-        default:
-            print("Нельзя выполнить это действие с этим автомобилем.")
-        }
-    }
-    
-    init(model: String, manufactureYear: Int, tankVolume: Double) {
-        self.model = model
-        self.manufactureYear = manufactureYear
-        self .tankVolume = tankVolume
-    }
-    
-}
+ extension Car {
+     mutating func switchWindows (action: WindowsStatus) {
+         if action == .open && windowsStatus == .open {
+             print("Окна уже открыты")
+         } else if action == .close && windowsStatus == .close {
+             print("Окна уже закрыты")
+         } else {
+             return windowsStatus = action
+         }
+     }
+ }
 
 
-class SportCar: Car {
-    var model: String
-    var manufactureYear: Int
-    var engineStatus: EngineStatus = .off
-    var windowsStatus: WindowsStatus = .close
-    let maxSpeed: Double
-    var tires: String
-    
-    func perform(action: Action) {
-        switch action {
-        case .changeTires(let newTires):
-            tires = newTires
-            print("Теперь стоят шины \(tires).")
-        default:
-            print("Нельзя выполнить это действие с этим автомобилем.")
-        }
-    }
-    
-    init(model: String, manufactureYear: Int, maxSpeed: Double, tires: String) {
-        self.model = model
-        self.manufactureYear = manufactureYear
-        self.maxSpeed = maxSpeed
-        self.tires = tires
-    }
-    
-}
+ class TankCar: Car {
+     var model: String
+     var manufactureYear: Int
+     var engineStatus: EngineStatus = .off
+     var windowsStatus: WindowsStatus = .close
+     let tankVolume: Double
+     var filledTankVolume: Double = 0
 
-extension TankCar: CustomStringConvertible {
-    var description: String {
-        return "\(model), \(manufactureYear) года выпуска.\nЦистерна заполнена на \(filledTankVolume) из \(tankVolume)."
-    }
-    
-    func printDescription(){
-        print(description)
-    }
-}
+     func perform(action: Action) {
+         switch action { //хотел использовать if, но он не понимает ни action == .fillTank, ни action == Action.fillTank, ни просто Action.fillTank
+         case .fillTank(let liquid):
+             let expectedVolume = liquid + filledTankVolume
+             if expectedVolume > tankVolume {
+                 print("Нет столько места")
+             } else if expectedVolume < 0 {
+                 break
+             } else {
+                 filledTankVolume = expectedVolume
+                 print("Цистерна заполена на \(filledTankVolume) из \(tankVolume).")
+             }
+         default:
+             print("Нельзя выполнить это действие с этим автомобилем.")
+         }
+     }
 
-extension SportCar: CustomStringConvertible {
-    var description: String {
-        return "\(model), \(manufactureYear)года выпуска.\nМаксимальная скорость — \(maxSpeed).\nУстановлены покрышки \(tires)."
-    }
-    
-    func printDescription(){
-        print(description)
-    }
-}
+     init(model: String, manufactureYear: Int, tankVolume: Double) {
+         self.model = model
+         self.manufactureYear = manufactureYear
+         self .tankVolume = tankVolume
+     }
+
+ }
 
 
-var tankCar1 = TankCar(model: "Scania", manufactureYear: 2015, tankVolume: 3000)
+ class SportCar: Car {
+     var model: String
+     var manufactureYear: Int
+     var engineStatus: EngineStatus = .off
+     var windowsStatus: WindowsStatus = .close
+     let maxSpeed: Double
+     var tires: String
 
-tankCar1.perform(action: .fillTank(2000))
-tankCar1.switchEngine(action: .on)
-tankCar1.printDescription()
+     func perform(action: Action) {
+         switch action {
+         case .changeTires(let newTires):
+             tires = newTires
+             print("Теперь стоят шины \(tires).")
+         default:
+             print("Нельзя выполнить это действие с этим автомобилем.")
+         }
+     }
+
+     init(model: String, manufactureYear: Int, maxSpeed: Double, tires: String) {
+         self.model = model
+         self.manufactureYear = manufactureYear
+         self.maxSpeed = maxSpeed
+         self.tires = tires
+     }
+
+ }
+
+ extension TankCar: CustomStringConvertible {
+     var description: String {
+         return "\(model), \(manufactureYear) года выпуска.\nЦистерна заполнена на \(filledTankVolume) из \(tankVolume)."
+     }
+
+     func printDescription(){
+         print(description)
+     }
+ }
+
+ extension SportCar: CustomStringConvertible {
+     var description: String {
+         return "\(model), \(manufactureYear) года выпуска.\nМаксимальная скорость — \(maxSpeed).\nУстановлены покрышки \(tires)."
+     }
+
+     func printDescription(){
+         print(description)
+     }
+ }
+
+
+ var tankCar1 = TankCar(model: "Scania   ", manufactureYear: 2015, tankVolume: 3000)
+ var tankCar2 = TankCar(model: "Камаз", manufactureYear: 2008, tankVolume: 2500)
+ tankCar1.perform(action: .fillTank(2000))
+ tankCar1.switchEngine(action: .on)
+ tankCar1.printDescription()
+ tankCar1.switchEngine(action: .on)
+ tankCar1.perform(action: .fillTank(3500))
+ tankCar2.printDescription()
+
+
+ var sportCar1 = SportCar(model: "Audi R8", manufactureYear: 2020, maxSpeed: 334, tires: "Michelin")
+ var sportCar2 = SportCar(model: "Lotus Evora", manufactureYear: 2010, maxSpeed: 280, tires: "Bridgestone")
+ sportCar1.switchWindows(action: .open)
+ sportCar1.switchWindows(action: .open)
+ sportCar1.perform(action: .changeTires("Goodyear"))
+ sportCar1.printDescription()
+ sportCar2.printDescription()
